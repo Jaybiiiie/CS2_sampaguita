3bd64bbfcc1cb3161f5c099911ab59b51cbea08e
Jaybiiiie <jtabino2031@wvc.pshs.edu.ph>
2026-09-01 09:06:07 +0800

    Add files via upload

---
+++ jayvee comsci formative assessment.py
@@ -0,0 +1,11 @@
+import math
+
+x1=float(input("enter x1"))
+x2=float(input("enter x2"))
+y1=float(input("enter y1"))
+y2=float(input("enter y2"))
+
+distance = math.sqrt(pow(x2-x1, 2)) + (pow(y2-y1, 2))
+print("the distance is", distance)
+
+
